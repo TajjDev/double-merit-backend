a86bee309119b45a2938dc7339bfbfdf6e778956
Khabab Tajudeen <doublemeritreceivemails@gmail.com>
2026-09-01 00:10:44 +0100

    Update Resend test recipient

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post("/api/contact", async (req, res) => {
     try {
       const data = await resend.emails.send({
         from: "Double Merit <onboarding@resend.dev>",
-        to: ["tajudeenkhabab2@gmail.com"],
+        to: ["doublemeritreceivemails@gmail.com"],
         replyTo: email,
         subject: `New Enquiry from ${firstName}`,
         html: `
