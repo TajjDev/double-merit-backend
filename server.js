791c8fba86e312ce197c81ae4b79123049f742e2
Khabab Tajudeen <doublemeritreceivemails@gmail.com>
2026-08-31 18:47:41 +0100

    Initial backend

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running ✅");
+});
+
+// Contact route
+app.post("/api/contact", async (req, res) => {
+  const { firstName, email, phone, message } = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        replyTo: email,
+        subject: `New Enquiry from ${firstName}`,
+        html: `
+          <h2>New Contact Form Enquiry</h2>
+      
+          <p><strong>Name:</strong> ${firstName}</p>
+          <p><strong>Email:</strong> ${email}</p>
+          <p><strong>Phone:</strong> ${phone || "Not provided"}</p>
+      
+          <hr/>
+      
+          <p><strong>Message:</strong></p>
+          <p>${message}</p>
+        `,
+      });
+
+    res.json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Email failed",
+    });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
