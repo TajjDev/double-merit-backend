78f249cd4175b187147a798d24b57c86a0c3bae7
Khabab Tajudeen <doublemeritreceivemails@gmail.com>
2026-08-31 20:41:08 +0100

    Show email error details

--- server.js
+++ server.js
@@ -47,10 +47,12 @@ app.post("/api/contact", async (req, res) => {
     res.json({ success: true });
 } catch (err) {
     console.error("EMAIL ERROR:", err);
+  
     res.status(500).json({
       success: false,
       message: "Email failed",
       error: err.message,
+      code: err.code,
     });
   }
 });
