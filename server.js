5132abda8bfef1a5a10bed2a79afc65ef4903105
Khabab Tajudeen <doublemeritreceivemails@gmail.com>
2026-08-31 19:55:43 +0100

    Use Render port

--- server.js
+++ server.js
@@ -53,7 +53,8 @@ app.post("/api/contact", async (req, res) => {
     });
   }
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
